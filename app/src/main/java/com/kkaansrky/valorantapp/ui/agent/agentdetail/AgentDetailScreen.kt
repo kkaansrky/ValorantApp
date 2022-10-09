953bd9f74628982c32d6b8f620708573114fdafb
Kaan Sarıkaya <kadirkaansarikaya@gmail.com>
2022-10-09 09:36:45 +0300

    AgentDetailScreen design v1(first basic ui)

--- app/src/main/java/com/kkaansrky/valorantapp/ui/agent/agentdetail/AgentDetailScreen.kt
+++ app/src/main/java/com/kkaansrky/valorantapp/ui/agent/agentdetail/AgentDetailScreen.kt
@@ -1,17 +1,25 @@
 package com.kkaansrky.valorantapp.ui.agent.agentdetail
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import coil.compose.rememberImagePainter
 import com.kkaansrky.valorantapp.data.entities.AgentResponse
 import com.kkaansrky.valorantapp.ui.status.ShowError
 import com.kkaansrky.valorantapp.ui.status.ShowLoading
+
 @Composable
 fun AgentDetailScreen(
     navController: NavController,
@@ -24,7 +32,7 @@ fun AgentDetailScreen(
     ) {
         val state = agentDetailViewModel.uiState.collectAsState().value
 
-        when(state){
+        when (state) {
             is AgentDetailUiState.Loading -> ShowLoading()
             is AgentDetailUiState.Success -> ShowAgent(state.agent)
             is AgentDetailUiState.Error -> ShowError()
@@ -33,6 +41,54 @@ fun AgentDetailScreen(
 }
 
 @Composable
-fun ShowAgent(agent: AgentResponse) {
-    Text(text = agent.data.displayName)
+fun ShowAgent(agent: AgentResponse) = with(agent.data) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(0.dp, 4.dp)
+            .background(Color.Red),
+        verticalArrangement = Arrangement.SpaceBetween,
+        horizontalAlignment = Alignment.CenterHorizontally,
+
+        ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(0.dp, 4.dp)
+        ) {
+            Image(
+                painter = rememberImagePainter(background),
+                contentDescription = null,
+                modifier = Modifier
+                    .fillMaxWidth(),
+                contentScale = ContentScale.Crop
+            )
+            Image(
+                painter = rememberImagePainter(fullPortrait),
+                contentDescription = null,
+                modifier = Modifier
+                    .fillMaxWidth(),
+                contentScale = ContentScale.Crop
+            )
+        }
+
+
+        Text(
+            text = displayName,
+            overflow = TextOverflow.Ellipsis,
+            maxLines = 1,
+            modifier = Modifier
+                .fillMaxWidth(),
+            textAlign = TextAlign.Center
+        )
+
+        Text(
+            text = developerName,
+            overflow = TextOverflow.Ellipsis,
+            maxLines = 1,
+            modifier = Modifier
+                .fillMaxWidth(),
+            textAlign = TextAlign.Center
+        )
+    }
 }
